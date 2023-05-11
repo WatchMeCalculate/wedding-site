1c3fc2e01141430b520e4312eacc0116a8936078
Jonah Jolley <jonah@devetry.com>
2023-05-10 23:03:44 -0600

    add nav

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,54 +3,79 @@ import { Link } from "@remix-run/react";
 import SvgMenuLine from "./icons/menu-line";
 import SvgCloseFill from "./icons/close-fill";
 
+/*
+<div onClick={() => setOpen(!open)} className="md:hidden">
+  {open ? <SvgCloseFill /> : <SvgMenuLine />}
+</div>;
+*/
 function Nav() {
   let [open, setOpen] = useState(false);
   return (
-    <nav className="flex flex-col md:flex-row bg-white hidden">
-      <div className="flex flex-wrap justify-between items-center">
-        <div className="sm:hidden">
-          <p className="font-mono font-semibold tracking-wide leading-loose">
-            R&J
-          </p>
-        </div>
-        <div onClick={() => setOpen(!open)} className="md:hidden">
-          {open ? <SvgCloseFill /> : <SvgMenuLine />}
+    <nav className="bg-grey-100 hidden">
+      <div className="px-2 mx-auto border border-red-400">
+        <div className="flex items-center justify-between space-x-4">
+          <div>
+            <Link to="/" className="md:hidden">
+              <span className="text-xl left-0">R & J</span>
+            </Link>
+          </div>
+          <div className="hidden md:block flex items-center space-x-6 py-4">
+            <Link to="/">
+              <span className="">Surprise!</span>
+            </Link>
+            <Link to="/">
+              <span className="">FAQ</span>
+            </Link>
+            <Link to="/">
+              <span className="">Schedule</span>
+            </Link>
+            <Link to="/">
+              <span className="">Travel</span>
+            </Link>
+            <Link to="/">
+              <span className="">Registry</span>
+            </Link>
+          </div>
+          <div>
+            <button
+              onClick={() => setOpen(!open)}
+              className="md:hidden flex py-4"
+            >
+              {open ? (
+                <SvgCloseFill className="w-6 h-6" />
+              ) : (
+                <SvgMenuLine className="h-6 w-6" />
+              )}
+            </button>
+          </div>
         </div>
       </div>
-      <ul
-        className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto  left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-          open ? "top-8 " : "top-[-490px]"
-        }`}
-      >
-        <li className="md:ml-8 text-xl md:my-0 my-7">
-          <Link
-            to="/"
-            className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-          />
-          Home
-        </li>
-        <li className="md:ml-8 text-xl md:my-0 my-7">
-          <Link
-            to="/"
-            className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-          />
-          Home
-        </li>
-        <li className="md:ml-8 text-xl md:my-0 my-7">
-          <Link
-            to="/"
-            className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-          />
-          Home
-        </li>
-        <li className="md:ml-8 text-xl md:my-0 my-7">
-          <Link
-            to="/"
-            className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-          />
-          Home
-        </li>
-      </ul>
+      {/* Mobile Menu */}
+      <div className={`${open ? "" : "hidden"} md:hidden`}>
+        <Link to="/">
+          <span className="block py-2 px-4 text-sm hover:bg-gray-100">
+            Surprise!
+          </span>
+        </Link>
+        <Link to="/">
+          <span className="block py-2 px-4 text-sm hover:bg-gray-100">FAQ</span>
+        </Link>
+        <Link to="/">
+          <span className="block py-2 px-4 text-sm hover:bg-gray-100">
+            Schedule
+          </span>
+        </Link>
+        <Link to="/">
+          <span className="block py-2 px-4 text-sm hover:bg-gray-100">
+            Travel
+          </span>
+        </Link>
+        <Link to="/">
+          <span className="block py-2 px-4 text-sm hover:bg-gray-100">
+            Registry
+          </span>
+        </Link>
+      </div>
     </nav>
   );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,9 +10,9 @@ export default function Index() {
   let now = new Date().getTime();
   var days = Math.floor((countDownDate - now) / (1000 * 60 * 60 * 24));
   return (
-    <div className="flex flex-row min-h-screen ">
+    <div className="grid-cols-1 min-h-screen ">
       <div className="flex-2">
-        <img src={Home} className="bg-contain" />
+        <img src={Home} className="h-96 md:h-full w-full object-cover" />
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2 items-center">
           <h1 className="text-center text-white text-5xl p-5">
             Rosemary and Jonah
